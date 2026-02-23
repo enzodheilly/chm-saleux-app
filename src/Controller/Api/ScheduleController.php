2cbccd82e7b145b2884f3ecd77c488afeb0571b5
DHEILLY Enzo <dheillyenzo@MacBook-Pro-de-DHEILLY.local>
2026-02-23 15:53:25 +0100

    Correction de bugs

--- src/Controller/Api/ScheduleController.php
+++ src/Controller/Api/ScheduleController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/schedule')]
 class ScheduleController extends AbstractController
